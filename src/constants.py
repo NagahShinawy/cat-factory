a720364052bb35bc32350e9cc458b83f2f4612f3
Nagah Shinawy <E-n.Shinawy@lean.sa>
2021-05-26 15:57:36 +0300

    download and display cats

---
+++ src/constants.py
@@ -0,0 +1,31 @@
+"""
+created by Nagaj at 12/05/2021
+"""
+
+# ###########   CATS  ##########################
+
+OUTPUT_FOLDER = "CATS"
+APPNAME = "CAT FACTORY APP"
+CAT_COUNTS = 8
+CAT_NAME = "lolcat{i}"
+
+# ###############  SERVICE  ###################
+
+CATS_URL = "http://consuming-python-services-api.azurewebsites.net/cats/random/"
+
+# ###########  commands #######################
+WINDOWS = "Windows"
+WINDOWS_COMMAND = "explorer"
+
+LINUX = "Linux"
+LINUX_COMMAND = "xdg-open"
+
+MACOS = "Darwin"
+MACOS_COMMAND = "open"
+
+
+# ###########  messages ##########################
+CREATE_FOLDER = "CREATING NEW FOLDER AT '{}'"
+NOT_SUPPORTED = "Don't Support Your OS '{}'"
+DOWNLOAD = "Downloading Cat: {}"
+DISPLAY = "Displaying Cats"
---
+++ src/utils.py
@@ -0,0 +1,9 @@
+"""
+created by Nagaj at 26/05/2021
+"""
+
+
+def print_the_header(appname):
+    print("-" * 70)
+    print(" " * 30 + appname)
+    print("-" * 70)
